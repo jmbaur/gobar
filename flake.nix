eb3d46bd34d91709fca399f055fdc644aad7a7e0
Jared Baur <jaredbaur@fastmail.com>
2022-01-27 19:13:39 -0800

    First commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "gobar";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }@inputs: {
+    overlay = final: prev: {
+      gobar = nixpkgs.legacyPackages.${prev.system}.buildGo117Module {
+        pname = "gobar";
+        version = "0.1.0";
+        CGO_ENABLED = 0;
+        src = builtins.path { path = ./.; };
+        vendorSha256 = "";
+      };
+    };
+  } // flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { overlays = [ self.overlay ]; inherit system; };
+    in
+    rec {
+      devShell = pkgs.mkShell {
+        buildInputs = [ pkgs.go_1_17 ];
+      };
+      packages.gobar = pkgs.gobar;
+      defaultPackage = packages.gobar;
+      apps.gobar = flake-utils.lib.mkApp { drv = pkgs.gobar; name = "gobar"; };
+      defaultApp = apps.gobar;
+    });
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jmbaur/gobar
+
+go 1.17
