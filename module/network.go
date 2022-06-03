ae5d2baa2fd8e65b2e38023f7cb025463633757a
Jared Baur <jaredbaur@fastmail.com>
2022-06-03 08:23:16 -0700

    Allow for pattern matching network interfaces

--- module/network.go
+++ module/network.go
@@ -1,8 +1,11 @@
 package module
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"regexp"
+	"strings"
 
 	col "github.com/jmbaur/gobar/color"
 	"github.com/jmbaur/gobar/i3"
@@ -10,50 +13,87 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-type Network struct {
-	// The name of the network interface
-	Interface string
+var (
+	ErrInvalidPattern = errors.New("invalid interface pattern string")
+	ErrNoMatch        = errors.New("no matching interface")
+)
 
+type iface struct {
 	link netlink.Link
 	ipv4 net.IP
 	ipv6 net.IP
 }
 
+type Network struct {
+	// The name of the network interface
+	Interface *string
+	Pattern   *string
+
+	ifaces []iface
+}
+
 func (n *Network) sendError(c chan Update, err error, position int) {
 	c <- Update{
 		Block: i3.Block{
-			FullText: fmt.Sprintf("%s: %s", n.Interface, err),
+			FullText: fmt.Sprintf("network: %s", err),
 			Color:    col.Red,
 		},
 		Position: position,
 	}
 }
 
+func (n *Network) valid() bool {
+	return (n.Pattern != nil && n.Interface == nil) ||
+		(n.Pattern == nil && n.Interface != nil)
+}
+
 func (n *Network) init() error {
-	if n.link == nil {
-		var err error
-		if n.link, err = netlink.LinkByName(n.Interface); err != nil {
+	if n.Pattern != nil {
+		links, err := netlink.LinkList()
+		if err != nil {
 			return err
 		}
-	}
-
-	v4addrs, err := netlink.AddrList(n.link, unix.AF_INET)
-	if err != nil {
-		return err
-	}
-	for _, addr := range v4addrs {
-		if chooseIPv4(addr.IP) {
-			n.ipv4 = addr.IP
+		matchedNone := true
+		for _, link := range links {
+			if matched, err := regexp.MatchString(*n.Pattern, link.Attrs().Name); matched && err == nil {
+				matchedNone = false
+				n.ifaces = append(n.ifaces, iface{
+					link: link,
+				})
+			}
 		}
+		if matchedNone {
+			return ErrNoMatch
+		}
+	} else if n.Interface != nil && n.ifaces == nil {
+		link, err := netlink.LinkByName(*n.Interface)
+		if err != nil {
+			return err
+		}
+		n.ifaces = append(n.ifaces, iface{
+			link: link,
+		})
 	}
 
-	v6addrs, err := netlink.AddrList(n.link, unix.AF_INET6)
-	if err != nil {
-		return err
-	}
-	for _, addr := range v6addrs {
-		if chooseIPv6(addr.IP, addr.Flags) {
-			n.ipv6 = addr.IP
+	for i, iface := range n.ifaces {
+		v4addrs, err := netlink.AddrList(iface.link, unix.AF_INET)
+		if err != nil {
+			return err
+		}
+		for _, addr := range v4addrs {
+			if chooseIPv4(addr.IP) {
+				n.ifaces[i].ipv4 = addr.IP
+			}
+		}
+
+		v6addrs, err := netlink.AddrList(iface.link, unix.AF_INET6)
+		if err != nil {
+			return err
+		}
+		for _, addr := range v6addrs {
+			if chooseIPv6(addr.IP, addr.Flags) {
+				n.ifaces[i].ipv6 = addr.IP
+			}
 		}
 	}
 
@@ -62,28 +102,32 @@ func (n *Network) init() error {
 
 func (n *Network) print(c chan Update, position int) {
 	var (
-		color    = col.Normal
-		fullText string
+		color            = col.Normal
+		fullTextUnjoined = []string{}
 	)
 
-	if n.ipv4 != nil || n.ipv6 != nil {
-		color = col.Green
+	for _, iface := range n.ifaces {
+		name := iface.link.Attrs().Name
+		if iface.ipv4 != nil || iface.ipv6 != nil {
+			color = col.Green
+		}
+
+		switch true {
+		case iface.ipv4 != nil && iface.ipv6 != nil:
+			fullTextUnjoined = append(fullTextUnjoined, fmt.Sprintf("%s: %s %s", name, iface.ipv4, iface.ipv6))
+		case iface.ipv4 != nil && iface.ipv6 == nil:
+			fullTextUnjoined = append(fullTextUnjoined, fmt.Sprintf("%s: %s", name, iface.ipv4))
+		case iface.ipv4 == nil && iface.ipv6 != nil:
+			fullTextUnjoined = append(fullTextUnjoined, fmt.Sprintf("%s: %s", name, iface.ipv6))
+		default:
+			fullTextUnjoined = append(fullTextUnjoined, fmt.Sprintf("%s: n/a", name))
+			color = col.Red
+		}
 	}
 
-	switch true {
-	case n.ipv4 != nil && n.ipv6 != nil:
-		fullText = fmt.Sprintf("%s: %s %s", n.Interface, n.ipv4, n.ipv6)
-	case n.ipv4 != nil && n.ipv6 == nil:
-		fullText = fmt.Sprintf("%s: %s", n.Interface, n.ipv4)
-	case n.ipv4 == nil && n.ipv6 != nil:
-		fullText = fmt.Sprintf("%s: %s", n.Interface, n.ipv6)
-	default:
-		fullText = fmt.Sprintf("%s: n/a", n.Interface)
-		color = col.Red
-	}
 	c <- Update{
 		Block: i3.Block{
-			FullText: fullText,
+			FullText: strings.Join(fullTextUnjoined, "; "),
 			Color:    color,
 		},
 		Position: position,
@@ -91,6 +135,10 @@ func (n *Network) print(c chan Update, position int) {
 }
 
 func (n *Network) Run(tx chan Update, rx chan i3.ClickEvent, position int) {
+	if valid := n.valid(); !valid {
+		n.sendError(tx, ErrInvalidPattern, position)
+	}
+
 	if err := n.init(); err != nil {
 		n.sendError(tx, err, position)
 		return
@@ -110,25 +158,27 @@ func (n *Network) Run(tx chan Update, rx chan i3.ClickEvent, position int) {
 	}
 
 	for update := range updates {
-		if update.LinkIndex == n.link.Attrs().Index {
-			if update.NewAddr {
-				if len(update.LinkAddress.IP) == net.IPv4len && chooseIPv4(update.LinkAddress.IP) {
-					n.ipv4 = update.LinkAddress.IP
-				} else if chooseIPv6(update.LinkAddress.IP, update.Flags) {
-					n.ipv6 = update.LinkAddress.IP
+		for i, iface := range n.ifaces {
+			if update.LinkIndex == iface.link.Attrs().Index {
+				if update.NewAddr {
+					if len(update.LinkAddress.IP) == net.IPv4len && chooseIPv4(update.LinkAddress.IP) {
+						n.ifaces[i].ipv4 = update.LinkAddress.IP
+					} else if chooseIPv6(update.LinkAddress.IP, update.Flags) {
+						n.ifaces[i].ipv6 = update.LinkAddress.IP
+					} else {
+						continue
+					}
 				} else {
-					continue
+					if iface.ipv4.Equal(update.LinkAddress.IP) {
+						n.ifaces[i].ipv4 = nil
+					} else if iface.ipv6.Equal(update.LinkAddress.IP) {
+						n.ifaces[i].ipv6 = nil
+					} else {
+						continue
+					}
 				}
-			} else {
-				if n.ipv4.Equal(update.LinkAddress.IP) {
-					n.ipv4 = nil
-				} else if n.ipv6.Equal(update.LinkAddress.IP) {
-					n.ipv6 = nil
-				} else {
-					continue
-				}
+				n.print(tx, position)
 			}
-			n.print(tx, position)
 		}
 	}
 }
