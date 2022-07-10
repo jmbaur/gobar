95240ef7d13ec7109046f8e982b31e2f4bb52cb8
Jared Baur <jaredbaur@fastmail.com>
2022-07-10 11:31:27 -0700

    Make verbose datetime not global to module

--- module/datetime.go
+++ module/datetime.go
@@ -6,8 +6,12 @@ import (
 
 	col "github.com/jmbaur/gobar/color"
 	"github.com/jmbaur/gobar/i3"
-	"golang.org/x/exp/slices"
 )
+
+type locationInfo struct {
+	loc     *time.Location
+	verbose bool
+}
 
 // Datetime is a module for printing the date and time.
 type Datetime struct {
@@ -17,29 +21,28 @@ type Datetime struct {
 	// toggled with a middle click.
 	ShowAllTimezones bool `mapstructure:"show_all_timezones"`
 
-	currentLocation *time.Location
-	locations       []*time.Location
+	currentLocation int
+	locations       []locationInfo
 	shortFormat     string
 	longFormat      string
-	verbose         bool
 }
 
 func (d *Datetime) print(tx chan []i3.Block, t time.Time) {
 	longFormat := d.longFormat
-	if !d.verbose {
-		longFormat = d.shortFormat
-	}
 
 	blocks := []i3.Block{}
 
 	if d.ShowAllTimezones {
-		for _, loc := range d.locations {
+		for _, locInfo := range d.locations {
+			if !locInfo.verbose {
+				longFormat = d.shortFormat
+			}
 			blocks = append(blocks, i3.Block{
 				Name:      "datetime",
-				Instance:  loc.String(),
-				FullText:  t.In(loc).Format(longFormat),
+				Instance:  locInfo.loc.String(),
+				FullText:  t.In(locInfo.loc).Format(longFormat),
 				Color:     col.Normal,
-				ShortText: t.In(loc).Format(d.shortFormat),
+				ShortText: t.In(locInfo.loc).Format(d.shortFormat),
 				MinWidth:  len(d.shortFormat),
 			})
 		}
@@ -47,9 +50,9 @@ func (d *Datetime) print(tx chan []i3.Block, t time.Time) {
 		blocks = []i3.Block{{
 			Name:      "datetime",
 			Instance:  "datetime",
-			FullText:  t.In(d.currentLocation).Format(longFormat),
+			FullText:  t.In(d.locations[d.currentLocation].loc).Format(longFormat),
 			Color:     col.Normal,
-			ShortText: t.In(d.currentLocation).Format(d.shortFormat),
+			ShortText: t.In(d.locations[d.currentLocation].loc).Format(d.shortFormat),
 			MinWidth:  len(d.shortFormat),
 		}}
 	}
@@ -58,7 +61,8 @@ func (d *Datetime) print(tx chan []i3.Block, t time.Time) {
 }
 
 func (d *Datetime) Run(tx chan []i3.Block, rx chan i3.ClickEvent) {
-	d.verbose = true
+	now := time.Now()
+
 	d.shortFormat = "15:04:05 MST"
 	d.longFormat = time.RFC1123
 
@@ -68,15 +72,25 @@ func (d *Datetime) Run(tx chan []i3.Block, rx chan i3.ClickEvent) {
 		d.longFormat = "Mon, 02 Jan 2006 15:04:05"
 	}
 
+	tzMap := map[string]struct{}{}
 	for _, tz := range d.Timezones {
 		loc, err := time.LoadLocation(tz)
 		if err != nil {
 			log.Printf("error parsing timezone: %v", err)
 		}
-		d.locations = append(d.locations, loc)
+		// avoid duplicate timezones
+		name, _ := now.In(loc).Zone()
+		if _, ok := tzMap[name]; ok {
+			continue
+		}
+		tzMap[name] = struct{}{}
+		d.locations = append(d.locations, locationInfo{
+			loc:     loc,
+			verbose: false,
+		})
 	}
 
-	d.currentLocation = d.locations[0]
+	d.currentLocation = 0
 
 	ready := make(chan struct{}, 1)
 	defer close(ready)
@@ -85,34 +99,28 @@ func (d *Datetime) Run(tx chan []i3.Block, rx chan i3.ClickEvent) {
 		ready <- struct{}{}
 	}()
 
-	now := time.Now()
-
 	for {
 		select {
 		case click := <-rx:
 			direction := 0
 			switch click.Button {
 			case i3.MiddleClick:
-				// TODO(jared): don't make this a global switch for all blocks
-				// in the module
-				d.verbose = !d.verbose
+				d.locations[d.currentLocation].verbose = !d.locations[d.currentLocation].verbose
 			case i3.LeftClick:
 				direction = 1
 			case i3.RightClick:
 				direction = -1
 			}
 
-			idx := slices.Index(d.locations, d.currentLocation)
-
-			idx += direction
-			if idx >= len(d.locations) {
-				idx = 0
-			} else if idx < 0 {
-				idx = len(d.locations) - 1
-			} else {
-				idx = 0 // fallback
+			if direction != 0 {
+				idx := d.currentLocation + direction
+				if idx >= len(d.locations) {
+					idx = 0
+				} else if idx < 0 {
+					idx = len(d.locations) - 1
+				}
+				d.currentLocation = idx
 			}
-			d.currentLocation = d.locations[idx]
 
 			d.print(tx, now)
 		case <-ready:
