dedc4b38a6f429c25ff47baf5c85b712d1a63033
jmbaur <jmbaur@users.noreply.github.com>
2022-11-27 00:37:00 +0000

    Update dependencies

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "gobar";
   version = "0.1.4";
   src = ./.;
-  vendorSha256 = "sha256-vDWU3XNEHxBVwxuxuZxGp3m4qxk85e8/Ullw04C7ihI=";
+  vendorSha256 = "sha256-q5owd4bnrvTzSAtM6WM+d80XU952M8U7x4KbPN5f4fE=";
   inherit CGO_ENABLED;
   ldflags = [ "-s" "-w" ];
 }
