85f2ad824038e31bac81fa19095a6f6719bb2a1a
Jared Baur <jaredbaur@fastmail.com>
2023-02-19 10:28:22 -0800

    Bump version

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { buildGoModule, ... }:
 buildGoModule {
   pname = "gobar";
-  version = "0.1.7";
+  version = "0.1.8";
   src = ./.;
   vendorSha256 = "sha256-kn3DFC/xd2gi1S14hUGygIMsttj8L+LwvGIF0POOa8Y=";
   ldflags = [ "-s" "-w" ];
