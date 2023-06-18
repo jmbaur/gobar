7ded3f15b8a170a18496b39268381b462d054ae1
jmbaur <jmbaur@users.noreply.github.com>
2023-06-18 01:19:02 +0000

    Update dependencies

--- default.nix
+++ default.nix
@@ -3,6 +3,6 @@ buildGoModule {
   pname = "gobar";
   version = "0.1.9";
   src = ./.;
-  vendorSha256 = "sha256-C/aTc/gDgd41scYWl3axaXThPpe+hzW9KRWD7qa2ykk=";
+  vendorSha256 = "sha256-B7G1Q5P+9R7MPcAf1UpXMSbjD9ou/hmKtFvT8X+epi0=";
   ldflags = [ "-s" "-w" ];
 }
