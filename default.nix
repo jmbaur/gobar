4b67a2f5dcd35bfb48d4dac5ab1f66d12e27ddd7
Jared Baur <jaredbaur@fastmail.com>
2023-11-18 07:19:05 -0800

    Use vendorHash

--- default.nix
+++ default.nix
@@ -3,6 +3,6 @@ buildGoModule {
   pname = "gobar";
   version = "0.1.9";
   src = ./.;
-  vendorSha256 = "sha256-tVu2auU1qfXDazMd/Id8PcIYlzRfQKXWPUICUuycamA=";
+  vendorHash = "sha256-tVu2auU1qfXDazMd/Id8PcIYlzRfQKXWPUICUuycamA=";
   ldflags = [ "-s" "-w" ];
 }
