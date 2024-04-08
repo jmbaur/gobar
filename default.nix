09ed7178275a675ac34b2b3b1c08993cc3ea1985
Jared Baur <jaredbaur@fastmail.com>
2024-04-08 00:41:39 -0700

    pre-commit-hooks -> git-hooks

--- default.nix
+++ default.nix
@@ -4,5 +4,8 @@ buildGoModule {
   version = "0.1.9";
   src = ./.;
   vendorHash = "sha256-yrl+/Fb55ysgHWvNOrfETm4mRnK7e3JfcseFapoTTh4=";
-  ldflags = [ "-s" "-w" ];
+  ldflags = [
+    "-s"
+    "-w"
+  ];
 }
--- flake.nix
+++ flake.nix
@@ -3,37 +3,78 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    pre-commit.url = "github:cachix/pre-commit-hooks.nix";
-    pre-commit.inputs.nixpkgs.follows = "nixpkgs";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: with inputs;
+  outputs =
+    inputs:
+    with inputs;
     let
-      forAllSystems = f: nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ] (system: f {
-        inherit system;
-        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
-      });
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs
+          [
+            "aarch64-linux"
+            "x86_64-linux"
+          ]
+          (
+            system:
+            f {
+              inherit system;
+              pkgs = import nixpkgs {
+                inherit system;
+                overlays = [ self.overlays.default ];
+              };
+            }
+          );
     in
     {
       overlays.default = _: prev: { gobar = prev.callPackage ./. { }; };
-      devShells = forAllSystems ({ pkgs, system, ... }: {
-        ci = pkgs.mkShell {
-          inputsFrom = [ pkgs.gobar ];
-          nativeBuildInputs = with pkgs; [ sway go-tools just nix-prefetch revive ];
-        };
-        default = self.devShells.${system}.ci.overrideAttrs (old: {
-          inherit (pre-commit.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              nixpkgs-fmt.enable = true;
-              govet.enable = true;
-              revive.enable = true;
-              gofmt.enable = true;
-            };
-          }) shellHook;
-        });
-      });
-      packages = forAllSystems ({ pkgs, ... }: { default = pkgs.gobar; });
-      apps = forAllSystems ({ pkgs, ... }: { default = { type = "app"; program = "${pkgs.gobar}/bin/gobar"; }; });
+      devShells = forAllSystems (
+        { pkgs, system, ... }:
+        {
+          ci = pkgs.mkShell {
+            inputsFrom = [ pkgs.gobar ];
+            nativeBuildInputs = with pkgs; [
+              sway
+              go-tools
+              just
+              nix-prefetch
+              revive
+            ];
+          };
+          default = self.devShells.${system}.ci.overrideAttrs (old: {
+            inherit
+              (git-hooks.lib.${system}.run {
+                src = ./.;
+                hooks = {
+                  nixfmt.enable = true;
+                  nixfmt.package = pkgs.nixfmt-rfc-style;
+                  govet.enable = true;
+                  revive.enable = true;
+                  gofmt.enable = true;
+                };
+              })
+              shellHook
+              ;
+          });
+        }
+      );
+      packages = forAllSystems (
+        { pkgs, ... }:
+        {
+          default = pkgs.gobar;
+        }
+      );
+      apps = forAllSystems (
+        { pkgs, ... }:
+        {
+          default = {
+            type = "app";
+            program = "${pkgs.gobar}/bin/gobar";
+          };
+        }
+      );
     };
 }
